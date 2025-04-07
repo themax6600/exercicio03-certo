0769fa3bc2aa83ab3502827aeeeda47702b15fc4
themax6600 <max.2935@aluno.pr.senac.br>
2025-04-07 12:26:00 -0300

    07/04/25

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,26 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJson('packge.json'),
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'dist/styles/main.css': 'src/styles/main.scss'
+                }
+            }
+        },
+        watch: {
+            sass: {
+                files: 'src/styles/**/*.scss',
+                tasks: ['sass']
+            }
+        }
     })
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['sass', 'watch']);
 }
